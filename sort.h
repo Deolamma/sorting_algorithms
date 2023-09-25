e54987ddc73847f42369a6b27c2b6035ded7ccb7
Khairat Adesina <khairatadesina01@gmail.com>
2023-09-25 21:12:02 +0100

    completed counting sort

--- sort.h
+++ sort.h
@@ -31,5 +31,7 @@ int partition(int *array, int start, int end, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort(int *array, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+int max(int *array, size_t size);
 
 #endif /*end #ifndef SORT_H */
